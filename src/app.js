0099630b5215b32e9ba3ccf90953e4239ec51b9a
Garcia <marlon.omar.g.garcia@accenture.com>
2019-04-05 17:06:31 -0300

    Completed the module

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ app.use(express.static(path.join(__dirname,'public')));
 const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'),'utf8');//archivo
 const accounts = JSON.parse(accountData);//archivo json valido
 
-const usersData = fs.readFileSync(path.join(__dirname,'json','users.json'),'utf8');
-const users = JSON.parse(usersData);
+const userData = fs.readFileSync(path.join(__dirname,'json','users.json'),'utf8');
+const users = JSON.parse(userData);
 
 //
 app.get('/',(req,res)=> res.render('index', {title:'Account Summary',accounts}));
