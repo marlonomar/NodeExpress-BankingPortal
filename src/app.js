ce95d6cdbb46d403805c6b09a2d3bbb74d3526c6
Garcia <marlon.omar.g.garcia@accenture.com>
2019-04-08 10:35:49 -0300

    Complete the module

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.post('/payment',(req,res)=>{
     accounts.credit.available += parseInt(req.body.amount,10);
     const accountsJSON = JSON.stringify(accounts,null,4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    res.render('/payment',{ message: "Payment Successful", account: accounts.credit });
+    res.render('payment',{ message: "Payment Successful", account: accounts.credit });
 
 });
 // aqui hacemos un proceso similar al anterior 
